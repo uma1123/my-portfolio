367a5bafe12ddafe003cbe69a75c2ff61a336f0b
uma1123 <yuma1596@gmail.com>
2024-09-23 13:19:37 +0900

    修正

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,31 +6,29 @@ export const metadata = {
   title: "About",
 };
 
-export default async function Page() {
+export default function AboutPage() {
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-gray-900 to-gray-800 text-white flex justify-center items-center overflow-x-hidden overflow-hidden">
-      <div className="w-full h-full p-8 bg-gray-900 rounded-lg shadow-lg flex flex-col transition-transform transform hover:scale-105">
+    <div className="h-screen w-full bg-gradient-to-r from-gray-900 to-gray-800 text-white flex justify-center items-center overflow-hidden">
+      <div className="w-full h-full p-8 bg-gray-900 rounded-lg shadow-lg flex flex-col justify-between transition-transform transform hover:scale-105 max-w-screen-lg mx-auto">
         {/* ヘッダー */}
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-4xl font-bold underline text-blue-400">YUMA</h1>
           <div className="space-x-4">
             <Link
               href="/"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               Home
             </Link>
             <Link
               href="/about"
-              scroll={false}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               About
             </Link>
             <Link
               href="/portfolio"
-              scroll={false}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               Portfolio
             </Link>
@@ -39,7 +37,7 @@ export default async function Page() {
 
         {/* メインコンテンツ */}
         <section className="mb-8 flex-grow">
-          <h1 className="text-4xl font-bold mb-4 text-center">About ME</h1>
+          <h2 className="text-4xl font-bold mb-4 text-center">About ME</h2>
           <p className="leading-relaxed text-center">
             はじめまして、ゆーまと申します。現在は芝浦工業大学に在学しながら、TechNovaに所属し、主にフロントエンドの勉強をしています。
             <br />
@@ -48,18 +46,22 @@ export default async function Page() {
         </section>
 
         {/* EXPERIENCES */}
-        <section className="mb-8">
-          <h2 className="text-2xl font-semibold mb-4">EXPERIENCES</h2>
-          <ul className="list-none space-y-2">
-            <li className="flex items-center">2023年3月</li>
-            <li className="flex items-center">2023年4月</li>
-            <li className="flex items-center">2027年3月</li>
+        <section className="mb-8 flex-grow">
+          <h2 className="text-4xl font-semibold mb-4 text-center">
+            EXPERIENCES
+          </h2>
+          <ul className="list-none space-y-2 text-center">
+            <li className="text-center">
+              2023年3月 埼玉県立越谷南高等学校 卒業
+            </li>
+            <li className="text-center">2023年4月 芝浦工業大学 入学</li>
+            <li className="text-center">2027年3月 芝浦工業大学 卒業見込み</li>
           </ul>
         </section>
 
         {/* Skills */}
         <section>
-          <h2 className="text-2xl font-semibold mb-4">Skills</h2>
+          <h2 className="text-4xl font-semibold mb-4 text-center">Skills</h2>
           <ul className="flex space-x-6 list-none justify-center">
             <li>
               <FaReact size={40} title="React" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,30 @@
 import { FaGithub, FaInstagram, FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-gray-900 to-gray-800 text-white flex justify-center items-center overflow-x-hidden overflow-y-hidden">
-      <div className="w-full h-full p-8 bg-gray-900 rounded-lg shadow-lg flex flex-col transition-transform transform hover:scale-105">
+    <div className="h-screen w-full bg-gradient-to-r from-gray-900 to-gray-800 text-white flex justify-center items-center overflow-hidden">
+      <div className="w-full h-full p-8 bg-gray-900 rounded-lg shadow-lg flex flex-col justify-between transition-transform transform hover:scale-105 max-w-screen-lg mx-auto">
         {/* ヘッダー */}
         <div className="flex justify-between items-center mb-8">
-          <h1 className="text-4xl font-bold underline text-blue-400">NAME</h1>
+          <h1 className="text-4xl font-bold underline text-blue-400">YUMA</h1>
           <div className="space-x-4">
             <Link
               href="/"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               Home
             </Link>
             <Link
               href="/about"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               About
             </Link>
             <Link
               href="/portfolio"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400 transition duration-200"
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
             >
               Portfolio
             </Link>
@@ -31,9 +32,9 @@ export default function Home() {
         </div>
 
         {/* メインコンテンツ部分 */}
-        <div className="flex justify-between items-center flex-grow">
-          <div className="w-1/2 bg-gray-700 p-6 rounded-lg">
-            <p className="text-white">
+        <div className="flex flex-col md:flex-row justify-between items-center flex-grow">
+          <div className="md:w-1/2 bg-gray-700 p-6 rounded-lg mb-6 md:mb-0 shadow-lg">
+            <p className="text-white text-lg leading-relaxed">
               こんにちは、ゆーまです。
               <br />
               大学2年生です。
@@ -41,9 +42,15 @@ export default function Home() {
               「たくさんのことを少しずつ」をモットーに生活しています。
             </p>
           </div>
-          <div className="w-1/2 flex justify-center items-center">
-            <div className="bg-gray-600 w-32 h-32 rounded-full">
-              {/* プロフィール画像を追加 */}
+          <div className="md:w-1/2 flex justify-center items-center">
+            {/* プロフィール画像を円形で表示 */}
+            <div className="w-40 h-40 relative overflow-hidden rounded-full border-4 border-blue-400 shadow-lg">
+              <Image
+                src="/snow.jpg"
+                alt="プロフィール"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
           </div>
         </div>
@@ -52,19 +59,19 @@ export default function Home() {
         <div className="flex justify-center space-x-8 mt-8">
           <a
             href="https://github.com/uma1123"
-            className="text-white hover:text-blue-400 transition duration-200"
+            className="text-white hover:text-blue-400 transition duration-300 transform hover:scale-125"
           >
             <FaGithub size={30} />
           </a>
           <a
             href="https://instagram.com/u_ma_11"
-            className="text-white hover:text-blue-400 transition duration-200"
+            className="text-white hover:text-blue-400 transition duration-300 transform hover:scale-125"
           >
             <FaInstagram size={30} />
           </a>
           <a
             href="https://twitter.com/uma11_11"
-            className="text-white hover:text-blue-400 transition duration-200"
+            className="text-white hover:text-blue-400 transition duration-300 transform hover:scale-125"
           >
             <FaXTwitter size={30} />
           </a>
